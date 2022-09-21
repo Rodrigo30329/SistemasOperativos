dc00f6ada7e8ead6eeed1174bb4f1041e303f499
Rodrigo30329 <joserodrigoavilacastro@gmail.com>
2022-09-21 10:27:02 -0500

    commit una vez

---
+++ EjercicioAlmacen/Almacen.py
@@ -0,0 +1,46 @@
+
+# En un Almacen hay 2 cajeros en que se pregunta si hay más de 5 personas. Se generan aleatoriamente y si hay 5 o más se abre una nueva caja.
+
+import threading
+import time
+from random import randrange
+from threading import Thread
+
+def Clientes():
+    N=randrange(100)
+    return N
+
+def LlegadaCaja():
+    contador = 1
+    Clientes = list()
+    Maximo = 6
+    while contador<Maximo:
+        Clientes.append('C'+str(contador))
+        print('La caja '+ str(contador) + ' tiene ' + str(contador) + ' clientes ')
+        if(contador==5):
+            print('La caja '+ str(contador) + ' llego a 5 clientes ')
+        contador+=1
+        cajero = Cajero()
+        cajero.start()
+        cp=cajero.comp
+        print(Clientes)
+        if (cp==True):
+            contador=contador-1
+            Clientes.pop(-1)
+            print(Clientes)
+            print('↨')
+
+class Cajero(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.comp = False
+    def run(self):
+        T=randrange(5)
+        time.sleep(T)
+        self.comp = True
+    
+def Distribuir():  
+    caja = threading.Thread(name='caja', target=LlegadaCaja)    
+    caja.start()
+
+Distribuir()
