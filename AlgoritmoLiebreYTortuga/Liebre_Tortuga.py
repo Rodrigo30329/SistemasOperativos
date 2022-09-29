ff69e0c049dd2d0551c47733d878ff2b517cb432
Rodrigo30329 <joserodrigoavilacastro@gmail.com>
2022-09-29 11:22:03 -0500

    funcional con 1 hilo

---
+++ AlgoritmoLiebreYTortuga/Liebre_Tortuga.py
@@ -0,0 +1,80 @@
+from random import randint
+import threading
+import time
+
+Completo=False
+Tortuga = "Tortuga"
+Liebre = "Liebre"
+
+AR=3
+R=6
+AL=1
+D=0
+GS=9
+RG=1
+PS=1
+RP=2
+
+EventoT = [AR, AR, AR, AR, AR, R, R, AL, AL, AL]
+EventoL = [D, D, GS, GS, RG, PS, PS, PS, RP, RP]
+
+
+def seleccionTortuga(ET):
+    N=randint(0, len(ET)-1)
+    return N
+
+def seleccionLiebre(EL):
+    N=randint(0, len(EL)-1)
+    return N
+
+def LiebrevsTortuga():
+    Completo=False
+    x=0
+    y=0
+    while Completo!=True:
+        ves=seleccionLiebre(EventoL)
+        ves2=seleccionTortuga(EventoT)
+        movl=EventoL[ves]
+        movt=EventoT[ves2]
+        if(x==0):
+            if(ves==4 or ves==8 or ves==9):
+                movl=1
+            else:
+                movl=movl
+        x=x+movl
+
+        if(y==0):
+            if(ves2==5 or ves2==6):
+                movt=1
+            else:
+                movt=movt
+        y=y+movt
+
+        time.sleep(1)
+
+        if(x>=200):
+            if(y<200):
+                print("La liebre ha llegado a 200")
+                print("La tortuga ha terminado en " + str(y))
+                Completo=True
+            if(y>=200):
+                print("Ha habido un empate")
+                Completo=True
+        elif(y>=200):
+            if(x<200):
+                print("La tortuga ha ganado")
+                print("La liebre ha terminado en " + str(x))
+                Completo=True
+            if(x>=200):
+                print("Ha habido un empate")
+                Completo=True
+        else:
+            print("La tortuga va en " + str(y))
+            print("La liebre va en " + str(x))
+            
+def Carrera():  
+    carrera = threading.Thread(name='carrera', target=LiebrevsTortuga)    
+    carrera.start()
+
+Carrera()
+
